4744bd0e8e6bda2bc5c051897be88dcd4f9e4b22
Jim Blackler <jimblackler@google.com>
2014-10-28 22:06:54 +0000

    License

--- scripts/game.js
+++ scripts/game.js
@@ -1,4 +1,13 @@
 "use strict";
+/*
+ Conway's Game of Life Demo copyright Jim Blackler 2014. All enquiries jimblackler@gmail.com
+
+ Conway's Game of Life Demo is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ */
+
 
 var SATURATION = 0.92;
 var VALUE = 0.88;
